3a10cdf4ecd68856e92eebf716f2933d7cac3874
Baumel Márton <marcibaumel@gmail.com>
2022-03-17 12:18:22 +0100

    signup componenet

--- MyShelf/src/app/app-routing.module.ts
+++ MyShelf/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { CollectionCreateComponent } from './collection-create/collection-create.component';
 import { CollectionListComponent } from './collection-list/collection-list.component';
-
 
 const routes: Routes = [
   { path: '', component: CollectionListComponent },
   { path: 'create', component: CollectionCreateComponent },
+  { path: 'user/signup', component: SignupComponent },
+  { path: 'user/login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [],
 })
 export class AppRoutingModule {}
--- MyShelf/src/app/auth/auth-data.model.ts
+++ MyShelf/src/app/auth/auth-data.model.ts
@@ -1,5 +1,5 @@
 export interface AuthData {
   email: string;
-  name: string;
+  username: string;
   password: string;
 }
