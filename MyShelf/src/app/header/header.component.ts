35689a56574f5bdec2b2a022e86df61a3162c987
Baumel Márton <marcibaumel@gmail.com>
2022-03-18 08:45:43 +0100

    header change

--- MyShelf/src/app/header/header.component.ts
+++ MyShelf/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  userIsAuthenticated = false;
+  private authListnerSubs: Subscription;
 
-  constructor() { }
+
+  constructor(private authService: AuthService){}
+
+
+  onLogout(){
+    this.authService.logout();
+  }
 
   ngOnInit(): void {
+    this,this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authListnerSubs = this.authService.getAuthStatusListnere().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
+
+  ngOnDestroy(): void {
+    this.authListnerSubs.unsubscribe();
+  }
+
 
 }
