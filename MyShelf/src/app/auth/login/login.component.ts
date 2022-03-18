ec720e9441f8d5536c07327fdfd35b47053eeee1
Baumel Márton <marcibaumel@gmail.com>
2022-03-18 08:35:56 +0100

    login

--- MyShelf/src/app/auth/login/login.component.ts
+++ MyShelf/src/app/auth/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  constructor(public authService: AuthService) {}
 
-  ngOnInit(): void {
+  onLogin(form: NgForm) {
+    if(form.invalid){
+      return
+    }
+    this.authService.login(form.value.email, form.value.username, form.value.password);
   }
 
 }
